91f392460464c7082f8f2009a8a76a1c3eee29f6
Abid Ansari <102030339+abidraza5594@users.noreply.github.com>
2023-03-21 20:35:34 +0530

    aa
    aa

--- src/components/Child.js
+++ src/components/Child.js
@@ -3,7 +3,7 @@ import React from "react";
 function Child(props) {
     return <div id="child">
             <p id="text">Today I am {props.age} Years of Age</p>
-            <button onClick={props.incrementage}>Increment</button>
+            <button id="button" onClick={props.incrementage}>Increment</button>
         </div>;
 }
 
